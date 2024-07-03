3c2d11198030e5b393c316a4bbea4e0b817a3b05
smthemex <138738845+smthemex@users.noreply.github.com>
2024-07-03 19:52:05 +0800

    Update __init__.py

--- ip_adapter/__init__.py
+++ ip_adapter/__init__.py
@@ -1,1 +1,10 @@
+from .ip_adapter import IPAdapter, IPAdapterPlus, IPAdapterPlusXL, IPAdapterXL, IPAdapterFull
+
+__all__ = [
+    "IPAdapter",
+    "IPAdapterPlus",
+    "IPAdapterPlusXL",
+    "IPAdapterXL",
+    "IPAdapterFull",
+]
 
